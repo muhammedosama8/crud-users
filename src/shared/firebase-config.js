937af64bc5d151a497d0f7c17c23451e8942ced9
Muhammed Osama <muhammed.o.nasser@gmail.com>
2022-12-09 16:10:32 +0200

    edit firebase

--- src/shared/firebase-config.js
+++ src/shared/firebase-config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "@firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCQnDQHgzVnWuV79ss0og4qbCZ-KrJxmC0",
-    authDomain: "crud-users-e2075.firebaseapp.com",
-    projectId: "crud-users-e2075",
-    storageBucket: "crud-users-e2075.appspot.com",
-    messagingSenderId: "826269478392",
-    appId: "1:826269478392:web:5cbae1573d0aff1db77a0d"
+    apiKey: "AIzaSyCnxzEq2JwPL6ryGZfZm8ve07G5ekXSBZI",
+    authDomain: "crud-852c6.firebaseapp.com",
+    projectId: "crud-852c6",
+    storageBucket: "crud-852c6.appspot.com",
+    messagingSenderId: "298746738579",
+    appId: "1:298746738579:web:1d4cf52894c2e387afda66"
 };
 
 // Initialize Firebase
